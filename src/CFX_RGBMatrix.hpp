ee0b77cea053181b82afc819cfdbb685ca2f9f55
Wouter van der Toorn <woutervandertoorn@gmail.com>
2018-12-04 21:29:17 +0100

    Created header for RGBMatrix

--- src/CFX_RGBMatrix.hpp
+++ src/CFX_RGBMatrix.hpp
@@ -20,3 +20,28 @@
 // The Custom FX library. If not, see <http://www.gnu.org/licenses/>.
 //
 ////////////////////////////////////////////////////////////////////////////////
+
+#ifndef CFX_RGBLedMatrix_H
+#define CFX_RGBLedMatrix_H
+
+#include <CFX_LedStripBase.hpp>
+#include <CFX_LedStrip.hpp>
+
+class CFX_RGBLedMatrix: public CFX_LedStrip
+{
+  public:
+    CFX_RGBLedMatrix(int pinnumber, uint16_t width, uint16_t height, int type = NEO_GRB + NEO_KHZ800);
+    
+    virtual void SetPixelColor(uint16_t x_pos, uint16_t y_pos, const CFX_Color& color);
+    virtual long GetPixelColorLong(uint16_t x_pos, uint16_t y_pos) const;
+    
+    virtual void SetPixelBrightness(uint16_t x_pos, uint16_t y_pos, uint8_t brightness);
+    virtual uint8_t GetPixelBrightness(uint16_t x_pos, uint16_t y_pos) const;
+
+  private:
+    uint16_t m_width;
+    uint16_t m_height;
+};
+
+
+#endif //CFX_RGBLedMatrix_H
